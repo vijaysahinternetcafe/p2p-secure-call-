56587be99f5e8be8d1e0376a566d012e4b9c5c3c
vijaysahinternetcafe <vijaysahinternetcafe@zohomail.in>
2026-08-10 22:52:33 +0530

    Implement SignalingMessage class with JSON support
    Added SignalingMessage class with JSON serialization.

--- lib/models/signaling_message.dart
+++ lib/models/signaling_message.dart
@@ -1,1 +1,33 @@
+import 'package:json_annotation/json_annotation.dart';
 
+part 'signaling_message.g.dart';
+
+@JsonSerializable()
+class SignalingMessage {
+  final String type;
+  final String? callId;
+  final String? from;
+  final String? to;
+  final String? sdp;
+  final String? candidate;
+  final String? sdpMid;
+  final int? sdpMLineIndex;
+  final int? timestamp;
+
+  SignalingMessage({
+    required this.type,
+    this.callId,
+    this.from,
+    this.to,
+    this.sdp,
+    this.candidate,
+    this.sdpMid,
+    this.sdpMLineIndex,
+    this.timestamp,
+  });
+
+  factory SignalingMessage.fromJson(Map<String, dynamic> json) =>
+      _\$SignalingMessageFromJson(json);
+
+  Map<String, dynamic> toJson() => _\$SignalingMessageToJson(this);
+}
