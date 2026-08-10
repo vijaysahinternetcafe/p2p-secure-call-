8dda5354844c0f46a1347c7d18faf9dae643568f
vijaysahinternetcafe <vijaysahinternetcafe@zohomail.in>
2026-08-10 22:50:44 +0530

    Initialize Flutter app with ChangeNotifierProvider
    Set up the main application structure with provider.

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
+import 'core/call_manager.dart';
+import 'screens/call_screen.dart';
+import 'screens/home_screen.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (_) => CallManager(),
+      child: MaterialApp(
+        title: 'P2P Secure Call',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: const HomeScreen(),
+      ),
+    );
+  }
+}
